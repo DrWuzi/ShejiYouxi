6377aaaa22c1ba7088647df69f8854641ca5b262
Dokkae6949 <finnliry@gmail.com>
2024-04-29 17:20:15 +0200

    Add toggle example

--- src/screen/settings.rs
+++ src/screen/settings.rs
@@ -1,11 +1,15 @@
-use iced::widget::{container, text};
+use iced::widget::{column, container, text, toggler};
 use iced::{Command, Element, Renderer, Theme};
 
 #[derive(Debug, Clone, Copy)]
-pub enum Message {}
+pub enum Message {
+    Toggled(bool),
+}
 
 #[derive(Debug, Clone, Default)]
-pub struct SettingsScreen {}
+pub struct SettingsScreen {
+    pub toggle: bool,
+}
 
 pub fn settings_screen() -> SettingsScreen {
     SettingsScreen::new()
@@ -13,14 +17,22 @@ pub fn settings_screen() -> SettingsScreen {
 
 impl SettingsScreen {
     pub fn new() -> Self {
-        Self {}
+        Self { toggle: false }
     }
 
-    pub fn update(&mut self, _message: Message) -> Option<Command<Message>> {
+    pub fn update(&mut self, message: Message) -> Option<Command<Message>> {
+        match message {
+            Message::Toggled(value) => self.toggle = value,
+        }
+
         None
     }
 
     pub fn view(&self) -> Element<'_, Message, Theme, Renderer> {
-        container(text("Settings Screen!")).into()
+        container(column!(
+            text("Settings Screen!"),
+            toggler("Toggle me!".to_string(), self.toggle, Message::Toggled),
+        ))
+        .into()
     }
 }
